94005fdfcbdfaaa6b1769271b2800aaa9ecb8c1b
Diwakar <diwakarnehrudasan@gmail.com>
2023-04-14 22:12:46 +0530

    diwa commit

--- rasa-frontend/src/App.js
+++ rasa-frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import ChatScreen from "./Components/ChatScreen";
 
+import React, { Component } from 'react';
+
 function App() {
   return (
     <div
